d44788c361c1765ebcf60bccb5b30a899b475209
Nadja Josefin Rünzel <nadja.ruenzel@gmail.com>
2023-08-23 12:51:13 +0200

    Update the Book Model with geocode feature and add  validation and create new Books with address in seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_102055) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_102959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_102055) do
     t.string "genre", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
