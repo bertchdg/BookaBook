7616c0f15040f61ad46802f54520979fd75783fa
Brett Hodge <brettchodge@gmail.com>
2023-08-24 13:23:53 +0200

    Implement Borrowing and Lending pages

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,7 +17,7 @@ class BookingsController < ApplicationController
   def set_book
     @book = Book.find(params[:book_id])
   end
-  
+
   def booking_params
     params.require(:booking).permit(:borrow_from, :return_on, :book_id)
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
-  def home
+  def lending
+    @books = Book.all
+    # @books = Book.where(current_user.id: booking.user_id)
+    @books = @books.select { |book| current_user.id == book.user_id }
+  end
+
+  def borrowing
+    @bookings = Booking.all
+    @bookings = @bookings.select { |booking| current_user.id == booking.user_id }
   end
 end
