4bbbbf4f4a015d17b9acd69e14fa74845befe472
Brett Hodge <brettchodge@gmail.com>
2023-08-25 15:13:07 +0200

    changed landing page back to books#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "books#index"
 
   resources :books, only: %i[index show new create edit update destroy] do
     resources :bookings, only: %i[create edit update show]
   end
-  
+
   resources :users, only: %i[new create edit update destroy]
 
   get 'lending', to: 'pages#lending'
